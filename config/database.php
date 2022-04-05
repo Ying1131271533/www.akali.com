94215c9dc3dbb93c99efd1435d4aeb4b6c11e1c9
akali <1131271533@qq.com>
2022-04-05 21:52:06 +0800

    修改数据连接

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '127.0.0.1',
+    'hostname'       => '124.71.218.160',
     // 数据库名
     'database'       => 'www_akali_com',
     // 用户名
-    'username'       => 'root',
+    'username'       => 'www_akali_com',
     // 密码
-    'password'       => '',
+    'password'       => 'C7TrGtZ87kMthbsY',
     // 端口
     'hostport'       => '',
     // 连接dsn
